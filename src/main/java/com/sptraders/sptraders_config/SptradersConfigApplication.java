9296537b36cf92d19e030c242f5cad0b5339ebb5
parthibaan416 <parthishankar416@outlook.com>
2025-04-15 15:13:23 +0530

    First commit

---
+++ src/main/java/com/sptraders/sptraders_config/SptradersConfigApplication.java
@@ -0,0 +1,13 @@
+package com.sptraders.sptraders_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptradersConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptradersConfigApplication.class, args);
+	}
+
+}
